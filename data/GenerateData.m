289800de1b7f35e531165904843580e7732e647a
Thomas Swearingen <swearin3@msu.edu>
2016-03-25 14:01:34 -0400

    change data generation

--- data/GenerateData.m
+++ data/GenerateData.m
@@ -1,28 +1,30 @@
 close all; clear; clc;
 
-ClusterSizes = [1e2 1e3 1e4 1e5 1e6];
-FeatureSizes = [1e2 1e3 1e4];
-SampleSizes = [1e2 1e3 1e4 1e5];
+ClusterSizes = [1e2 1e3 1e4];
+FeatureSizes = [1e2 5e2 1e3];
+SampleSizes = [1e2 5e2 1e3];
+AvgClusterDistances = [1e2 5e2 1e3];
+MaxIter = 100000;
 
 for NumClusters = ClusterSizes
     for NumSamples = SampleSizes
         for NumFeatures = FeatureSizes
-            
-            fprintf('Generating Data with NumCluster=%g, NumSamples=%g, and NumFeatures=%g\n', NumClusters, NumSamples, NumFeatures);
-            AvgClusterDistance = NumFeatures * 1000;
-            
-            clustermeans = GenerateMeans(NumClusters, NumFeatures, AvgClusterDistance, AvgClusterDistance, 10000);
-            
-            
-            for cluster = 1:NumClusters
-                clustercov{cluster} = 0.45*rand(NumFeatures);
-                clustercov{cluster} = clustercov{cluster}' * clustercov{cluster}; %make psd
+        	for AvgClusterDistance = AvgClusterDistances
+				fprintf('Generating Data with NumCluster=%g, NumSamples=%g, NumFeatures=%g, and AvgClusterDistance = %g\n', NumClusters, NumSamples, NumFeatures, AvgClusterDistance);
+			
+				clustermeans = GenerateMeans(NumClusters, NumFeatures, AvgClusterDistance, AvgClusterDistance, MaxIter);
+			
+			
+				for cluster = 1:NumClusters
+					clustercov{cluster} = 0.45*rand(NumFeatures);
+					clustercov{cluster} = clustercov{cluster}' * clustercov{cluster}; %make psd
 
-                data{cluster} = mvnrnd(clustermeans(cluster,:)', clustercov{cluster}, NumSamples);
+					data{cluster} = mvnrnd(clustermeans(cluster,:)', clustercov{cluster}, NumSamples);
+				end
+			
+				fname = sprintf('Data_%gclusters_%gsamples_%gfeatures_%gclustdist.mat', NumClusters, NumSamples, NumFeatures, AvgClusterDistance);
+				save(fname,'data', 'clustercov', 'clustermeans', 'NumClusters', 'NumSamples', 'NumFeatures', '-v7.3');
             end
-            
-            fname = sprintf('Data_%gclusters_%gsamples_%gfeatures.mat', NumClusters, NumSamples, NumFeatures);
-            save(fname,'data', 'clustercov', 'clustermeans', 'NumClusters', 'NumSamples', 'NumFeatures', '-v7.3');
         end
     end
 end
