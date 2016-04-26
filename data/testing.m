a1ef37d0e8ba09fcc9d7daa01091fe3721728a7a
Ben Frey <spartanhooah@users.noreply.github.com>
2016-04-26 14:25:07 -0400

    Modified testing script to record centroid locations.

--- data/testing.m
+++ data/testing.m
@@ -6,8 +6,8 @@ for i = 1:size(files)
     load(name);
     c_locations = strfind(name, 'c');
     k = str2num(name(6:c_locations(1) - 1));
-    [yy_idx, yy_numiter, yy_timer] = yykmeans(alldata, k, k/10, maxiter);
-    [km_idx, km_numiter, ~, ~, km_timer ] = simple_kmeans(alldata, k, maxiter, alldata(1:k, :));
+    [yy_idx, yy_locations, yy_numiter, yy_timer] = yykmeans(alldata, k, k/10, maxiter);
+    [km_idx, km_numiter, km_locations, ~, km_timer ] = simple_kmeans(alldata, k, maxiter, alldata(1:k, :));
     save(strcat('results_', name, '.mat'), 'yy_idx', 'yy_numiter', 'yy_timer',...
         'km_idx', 'km_numiter', 'km_timer');
 end
