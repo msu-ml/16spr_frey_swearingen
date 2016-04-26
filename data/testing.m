3d0fd2d496b3d1816923a87852d6b7a1d9e5a5a0
Ben Frey <spartanhooah@gmail.com>
2016-04-26 11:09:07 -0400

    Updated testing script.

--- data/testing.m
+++ data/testing.m
@@ -1,10 +1,6 @@
 clear
 maxiter = 1000;
 files = dir('Data*.mat');
-yy_iterations = [];
-yy_times_means = [];
-km_iterations = [];
-km_times_means = [];
 for file = files
     name = file.name;
     load(name);
@@ -12,9 +8,6 @@ for file = files
     k = str2num(name(6:c_locations(1) - 1));
     [yy_idx, yy_numiter, yy_timer] = yykmeans(alldata, k, k/10, maxiter);
     [km_idx, km_numiter, ~, ~, km_timer ] = simple_kmeans(alldata, k, maxiter, alldata(1:k, :));
-    yy_iterations = [yy_iterations; yy_numiter];
-    yy_times_means = [yy_times_means; mean(yy_timer)];
-    km_iterations = [km_iterations; km_numiter];
-    km_times_means = [km_times_means; mean(km_timer)];
+    save(strcat('results_', name, '.mat'), 'yy_idx', 'yy_numiter', 'yy_timer',...
+        'km_idx', 'km_numiter', 'km_timer');
 end
-
